a6ca7cbfd7b669d9983a3fb6b9256a251961f533
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2025-11-15 23:27:52 +0530

    Add logic to calculate moves in 'Arrival of the General'

---
+++ Arrival of the General.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+max_index = a.index(max(a))
+min_index = n - 1 - a[::-1].index(min(a))
+
+moves = max_index + (n - 1 - min_index)
+
+if min_index < max_index:
+    moves -= 1
+
+print(moves)
